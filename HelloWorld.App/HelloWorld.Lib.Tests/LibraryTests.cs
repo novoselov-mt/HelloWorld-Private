4b90e9a7bdd50b07c8bb1d554ba4a69e3c2c0998
knov <knov@apmaldi.com>
2014-12-16 12:00:33 -0500

    Library and corresponding unit tests have been added

---
+++ HelloWorld.App/HelloWorld.Lib.Tests/LibraryTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorld.Lib.Tests
+{
+    [TestClass]
+    public class LibraryTests
+    {
+        [TestMethod]
+        public void TestGreetings()
+        {
+            var lib = new Library();
+
+            Assert.AreEqual("Hello", lib.GetGreetings());
+        }
+    }
+}

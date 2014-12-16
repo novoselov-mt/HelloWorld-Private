3bdd34306fb203f72fe7764d3d57fe0d8b53ce57
knov <knov@apmaldi.com>
2014-12-16 12:06:51 -0500

    Some file move (stupid VS)

---
+++ HelloWorld.Lib/Library.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld.Lib
+{
+    public class Library
+    {
+        public string GetGreetings()
+        {
+            return string.Empty;
+        }
+    }
+}

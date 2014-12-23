ad176aeaada5e0278d524d0bd794a7b7ed44f6a9
knov <knov@apmaldi.com>
2014-12-23 16:11:34 -0500

    Add missed files

---
+++ HelloWorld/HelloWorld-cs-lib/Library.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld_cs_lib
+{
+    public class Library
+    {
+        public void Init()
+        {
+            
+        }
+
+        public void Release()
+        {
+            
+        }
+    }
+}

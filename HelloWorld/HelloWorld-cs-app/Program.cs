4159b4cd8094306dd424c9b719e832877cbefa28
knov <knov@apmaldi.com>
2014-12-23 14:00:08 -0500

    CS app with nuget staff added

---
+++ HelloWorld/HelloWorld-cs-app/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld_cs_app
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            log.Info("Hello log4net");
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
